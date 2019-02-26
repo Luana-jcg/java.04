8a41940abcf7cafcc7d8e6e36049ca4d1864299a
LUANA JESUS DE CARVALHO GALDINO <G.LUANA@csi.aneas.msft>
2019-02-26 20:20:15 -0300

    aula 04 de POO II

---
+++ src/TestaWhile.java
@@ -0,0 +1,12 @@
+public class TestaWhile {
+
+	public static void main(String[] args) {
+		int i = 5;
+		while(i > 0) {
+			System.out.println("Oi");
+			i--;
+		}
+
+	}
+
+}
---
+++ src/TesteCondicional.java
@@ -0,0 +1,13 @@
+public class TesteCondicional {
+
+	public static void main(String[] args) {
+		int idade = 17;
+		int acomp = 3;
+		if(idade >= 18 || acomp >= 3) {
+			System.out.print("você pode entrar");
+		}
+		else {
+			System.out.print("você não pode entrar");
+		}
+	}
+}
